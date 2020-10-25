b4ed3aeca2fa8d5951e0be1fa0e8e0a57bfe927b
Seunghwan <seunghwansohn@gmail.com>
2020-10-25 18:07:51 +0700

    2. prisma 클라이언트 및 서버와 graphql 서버 설치

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import bcrypt from 'bcryptjs';
+import * as jwt from "jsonwebtoken";
 
+import {GraphQLServer} from 'graphql-yoga'
+import { prisma } from './prisma/generated/prisma-client'
+
+
+//비밀번호 및 토큰 생성 부분
 const password = 'pleaseDontHackMe3248';
 console.log('Raw Password: ', password);
 
@@ -13,3 +19,26 @@ bcrypt.hash(password, 8)
     return doesMatch;
   }
   ).then(doesMatch => console.log('Password Matches: ', doesMatch));
+
+const token = jwt.sign({ name: 'Someone' }, 'anotherSecret', { expiresIn: '1 day' });
+console.log('토큰', token);
+
+const decrypted = jwt.verify(token, 'anotherSecret');
+console.log('디크립트토큰', decrypted);
+
+
+
+//GraphQL 서버 부분
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+server.start(() => console.log('Server is running on localhost:4000'))
